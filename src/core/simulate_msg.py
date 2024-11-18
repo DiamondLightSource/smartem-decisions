9b0b5ec320fb4d60828345fbd1c9913540c303f9
Val Redchenko <val.redchenko@diamond.ac.uk>
2024-11-18 16:27:23 +0000

    simulate legit and non-legit messages for particle picking, particle selection and motion correction complete events, ack or nack on the consumer

--- src/core/simulate_msg.py
+++ src/core/simulate_msg.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
+
 import os
 import pika
 import typer
 import yaml
+import json
 from dotenv import load_dotenv
-load_dotenv()
-from schemas import mq_event
 
+from schemas.mq_event import MessageQueueEventType
+
+load_dotenv()
+from uuid import uuid4
 
 simulate_msg_cli = typer.Typer()
 conf = yaml.safe_load(open(os.path.join(os.path.dirname(__file__), 'config.yaml')))
@@ -21,65 +25,114 @@ connection = pika.BlockingConnection(
         )
     ))
 
-@simulate_msg_cli.command()
-def rouge_message(legit: bool = True):
-    """Simulate an ill-formed message with no valid recipient, i.e. headers
-    fail to specify a valid `event_type` making it impossible to route the
-    message to the correct consumer.
-    """
-    print('Not implemented')
-    pass
-
-
-@simulate_msg_cli.command()
-def external_session_start(legit: bool = True):
-    """Because the session is started manually by the user of EPU software,
-    it's origin is external to the system.
-    """
-    print('Not implemented')
-    pass
-
-@simulate_msg_cli.command()
-def external_grid_scan_start(legit: bool = True):
-    """Simulate a grid scan start event (external to the system)
-    """
-    print('Not implemented')
-    pass
-
-@simulate_msg_cli.command()
-def external_grid_scan_complete(legit: bool = True):
-    """Simulate a grid scan complete event (external to the system)
-    """
-    print('Not implemented')
-    pass
-
-@simulate_msg_cli.command()
-def motion_correct_and_ctf_start(legit: bool = True):
-    """Simulate a motion capture and CTF start message to test workflow response
-    """
-    print('Not implemented')
-    pass
-
-@simulate_msg_cli.command()
-def motion_correct_and_ctf_complete(legit: bool = True):
-    """Simulate a Motion capture and CTF complete message to test workflow response
-    """
+def _send_msg(msg):
     channel = connection.channel()
     channel.queue_declare(queue=conf['rabbitmq']['queue_name'], durable=True)
-    headers = mq_event.MessageQueueEventHeaders(
-        event_type=mq_event.MessageQueueEventType.session_start
-    )
-    message = {}
     channel.basic_publish(
         exchange='',
         routing_key=conf['rabbitmq']['routing_key'],
-        body=str(message),
+        body=json.dumps(msg),
         properties=pika.BasicProperties(
             delivery_mode=pika.DeliveryMode.Persistent
         ))
-    print(f" [x] Sent {message}")
+    print(f" [x] Sent {msg}")
     connection.close()
-    pass
+
+
+@simulate_msg_cli.command()
+def rogue_message(event_type_missing: bool = False):
+    """Simulate an ill-formed message with no valid recipient,
+    either `event_type` parameter taking on an unrecognised value or missing entirely -
+    making it impossible to route the message to the correct consumer.
+    """
+    message = {
+        'event_type': 'foo bar',
+    } if event_type_missing else {
+        'micrograph_id': str(uuid4()),
+        'total_motion': 0.123,
+        'average_motion': 0.006,
+        'ctf_max_resolution_estimate': 0.123123,
+    }
+    _send_msg(message)
+
+@simulate_msg_cli.command()
+def external_session_start(legit: bool = True):
+    print('Not implemented')
+
+@simulate_msg_cli.command()
+def external_grid_scan_start(legit: bool = True):
+    print('Not implemented')
+
+@simulate_msg_cli.command()
+def external_grid_scan_complete(legit: bool = True):
+    print('Not implemented')
+
+@simulate_msg_cli.command()
+def motion_correction_start(legit: bool = True):
+    print('Not implemented')
+
+@simulate_msg_cli.command()
+def motion_correction_complete(legit: bool = True):
+    message = {
+        'event_type': str(MessageQueueEventType.motion_correction_complete.value),
+        'micrograph_id': str(uuid4()),
+        'total_motion': 0.123,
+        'average_motion': 0.006,
+        'ctf_max_resolution_estimate': 0.123123,
+    } if legit else {
+        'event_type': str(MessageQueueEventType.motion_correction_complete.value),
+        'micrograf_id': 'xx',
+        'total_lotion': None,
+        'averag_emotion': -0.006,
+        'ctf_max_revolution_estimate': '0.123123',
+    }
+    _send_msg(message)
+
+@simulate_msg_cli.command()
+def particle_picking_start(legit: bool = True):
+    print('Not implemented')
+
+@simulate_msg_cli.command()
+def particle_picking_complete(legit: bool = True):
+    message = {
+        'event_type': str(MessageQueueEventType.particle_picking_complete.value),
+        'micrograph_id': str(uuid4()),
+        'number_of_particles_picked': 10,
+        'pick_distribution': {} # TODO
+    } if legit else {
+        'event_type': str(MessageQueueEventType.particle_picking_complete.value),
+        'micrograf_id': 'xx',
+        'number_of_particles_picked': -10,
+        'pick_distribution': None,
+    }
+    _send_msg(message)
+
+@simulate_msg_cli.command()
+def particle_selection_start(legit: bool = True):
+    print('Not implemented')
+
+@simulate_msg_cli.command()
+def particle_selection_complete(legit: bool = True):
+    message = {
+        'event_type': str(MessageQueueEventType.particle_selection_complete.value),
+        'micrograph_id': str(uuid4()),
+        'number_of_particles_selected': 0,
+        'selection_distribution': {}  # TODO
+    } if legit else {
+        'event_type': str(MessageQueueEventType.particle_selection_complete.value),
+        'micrograf_id': 'xx',
+        'number_of_particles_selected': -10,
+        'selection_distribution': None,
+    }
+    _send_msg(message)
+
+@simulate_msg_cli.command()
+def ctf_start(legit: bool = True):
+    print('Not implemented')
+
+@simulate_msg_cli.command()
+def ctf_complete(legit: bool = True):
+    print('Not implemented')
 
 if __name__ == "__main__":
     simulate_msg_cli()
