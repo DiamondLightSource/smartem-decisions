1c29793198bbbce84d59e7e55bda23b6cfeb73dc
vredchenko <vredchenko@users.noreply.github.com>
2025-08-26 08:54:11 +0000

    deploy: d7f9dfdc7394caf02ab0250a8979f57f3a3595c9

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev311+g8d09350b6.d20250824',
+    VERSION: '0.1.dev312+gd7f9dfdc7.d20250826',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
