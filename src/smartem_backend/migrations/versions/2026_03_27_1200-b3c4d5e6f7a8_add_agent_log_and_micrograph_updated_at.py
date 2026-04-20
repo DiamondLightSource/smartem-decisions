3a5577dc2ffa7e3d4fe91b25f648b898a73c0a45
Val Redchenko <lazyval@gmail.com>
2026-04-20 13:54:44 +0100

    fix(migrations): rebase agent_log migration onto consolidated foilhole head
    After merging PR #243, the head revision advanced from a1b2c3d4e5f6 to
    b2c3d4e5f6a7 (consolidated foilhole groups migration). Repoint this
    branch's migration at the new head so alembic sees a single linear chain
    instead of two divergent heads.

--- src/smartem_backend/migrations/versions/2026_03_27_1200-b3c4d5e6f7a8_add_agent_log_and_micrograph_updated_at.py
+++ src/smartem_backend/migrations/versions/2026_03_27_1200-b3c4d5e6f7a8_add_agent_log_and_micrograph_updated_at.py
@@ -1,7 +1,7 @@
 """Add agent_log table and micrograph.updated_at column
 
 Revision ID: b3c4d5e6f7a8
-Revises: a1b2c3d4e5f6
+Revises: b2c3d4e5f6a7
 Create Date: 2026-03-27 12:00:00.000000
 
 """
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision = "b3c4d5e6f7a8"
-down_revision = "a1b2c3d4e5f6"
+down_revision = "b2c3d4e5f6a7"
 branch_labels = None
 depends_on = None
 
